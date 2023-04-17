dcfa2dd23c2fb8aae0b97a83a57012451f550dce
letarcioCastro <Letarcio.castro70@gmail.com>
2023-04-16 21:37:05 -0300

    ajustando tabela

--- src/pages/api/create.ts
+++ src/pages/api/create.ts
@@ -1,17 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../lib/prima";
+
 type Data = {
   title: string;
   author: string;
   link_url: string;
   image_url: string;
+  isPdf: boolean;
+  quantity: number;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { author, image_url, link_url, title }: Data = req.body;
+  const { author, image_url, link_url, title, isPdf, quantity }: Data = req.body;
 
   try {
     if (!author || !image_url || !link_url || !title) {
@@ -23,6 +26,8 @@ export default async function handler(
         image_url,
         link_url,
         title,
+        isPdf,
+        quantity,
       },
     });
     res.status(200).json({ message: "Livro adicionado!", data: req.body });
